132219ca6e71ae5fb1b37634c47b53a43a29d6eb
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-04 13:59:24 +0600

    Fetch data from database and get is help of user variable

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const app = Vue.createApp({
     data() {
        return {
-          
+          users: [],
        }
    },
    methods: {
-      
+    
+   },
+   mounted() {
+      axios
+         .get('http://localhost/crud/api.php?action=read')
+            .then(response => (this.users = response.data.users))
    },
    
 })
 app.component('', {
    props: [],
   template:``, 
-}).mount('#app')
+})
+app.mount('#app')
