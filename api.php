336cea771e7f00f64dff2543a6c37d23fde98bd2
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-04 13:07:23 +0600

    Run a query for catch datafrom database

--- api.php
+++ api.php
@@ -7,9 +7,11 @@ if (isset($_GET['action'])) {
    $action = $_GET['action'];
 }
 
-
+if ($action == 'read') {
+   $result = $connection->query("SELECT * FROM  `users`");
+}
 $response = [
-   ''
+   '',
 ];
 header('content-type: application/json');
 echo json_encode($response);
